f4cee5d7ecf079e6a5b6b3574d2db308979777b1
w3prog <w3prog@gmail.com>
2016-11-26 03:36:16 +0300

    Добавил недостающий return

--- GoodTravel/app/src/main/java/ru/osll/goodtravel/models/Place.java
+++ GoodTravel/app/src/main/java/ru/osll/goodtravel/models/Place.java
@@ -66,7 +66,7 @@ public class Place extends RealmObject {
     }
 
     public PlaceTypeEnum getCategory() {
-        PlaceTypeEnum.valueOf(category);
+        return PlaceTypeEnum.valueOf(category);
     }
 
     public void setCategory(PlaceTypeEnum category) {
